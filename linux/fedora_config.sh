34cabf7d22d91f139f22be238660044c18f09e23
Rafaelszc <rafaelbjj84@gmail.com>
2025-03-07 01:34:53 -0300

    refrac: remove anaconda sh file after install

--- linux/fedora_config.sh
+++ linux/fedora_config.sh
@@ -141,3 +141,5 @@ wget https://repo.anaconda.com/archive/Anaconda3-2024.10-1-Linux-x86_64.sh
 chmod +x Anaconda3-2024.10-1-Linux-x86_64.sh
 
 ./Anaconda3-2024.10-1-Linux-x86_64.sh
+
+rm -rf /Anaconda3-2024.10-1-Linux-x86_64.sh
