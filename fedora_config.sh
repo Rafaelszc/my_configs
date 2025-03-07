8024d90ffa93e7cf763aae35349380eb9193b1a2
Rafaelszc <rafaelbjj84@gmail.com>
2025-03-07 01:17:02 -0300

    add: sh file to configure fedora linux

---
+++ fedora_config.sh
@@ -0,0 +1,135 @@
+#!/bin/bash
+
+# Um arquivozinho com as configs que eu gosto do fedora
+# Sei que o código pode estar porco, mas depois melhoro
+#
+# Divirta-se!
+#
+# Author: Rafaelszc
+# Github: https://github.com/Rafaelszc
+#
+
+# Pyhton pip packages
+
+PIP_PACKAGES=(
+	"requests"
+	"beautifulsoup4"
+	"selenium"
+	"scikit-learn"
+	"pandas"
+	"seaborn"
+	"discord"
+	"flask"
+)
+
+# Flatpak programs list
+
+FLATPAK_PROGRAMS=(
+	"app.zen_browser.zen"
+	"com.github.johnfactotum.Foliate"
+	"com.google.AndroidStudio"
+	"com.jeffser.Alpaca"
+	"com.jetbrains.IntelliJ-IDEA-Community"
+	"com.obsproject.Studio"
+	"com.spotify.Client"
+	"com.valvesoftware.Steam"
+	"dev.vencord.Vesktop"
+	"io.github.alainm23.planify"
+	"io.gitlab.news_flash.NewsFlash"
+	"io.missioncenter.MissionCenter"
+	"md.obsidian.Obsidian"
+	"org.gnome.Builder"
+	"org.qbittorrent.qBittorrent"
+	"org.kde.krita"
+)
+
+# List of GNOME extensions repo
+
+GNOME_EXTENSIONS=(
+	"https://github.com/stuarthayhurst/alphabetical-grid-extension.git"
+	"https://github.com/eonpatapon/gnome-shell-extension-caffeine.git"
+	"https://github.com/Tudmotu/gnome-shell-extension-clipboard-indicator.git"
+	"https://github.com/tuberry/extension-list.git"
+)
+
+# Orchis Theme repo
+
+THEME_URL="https://github.com/vinceliuice/Orchis-theme.git"
+
+# Functions to install some things
+
+get_repo_name () {
+        reponame=$(basename "$1" .git)
+        echo $reponame
+}
+
+install_extension () {
+	reponame=$(get_repo_name "$1")
+
+	git clone "$1"
+	cd "$reponame"
+	make all
+	cd ..
+	rm -rf "$reponame"
+}
+
+# Setting GNOME config
+
+## Theme
+
+dconf write /org/gnome/desktop/interface/color-scheme "'prefer-dark'"
+
+## Power profile
+
+dconf write /org/gnome/shell/last-selected-power-profile "'performance'"
+
+## Keyboard and mouse
+
+dconf write /org/gnome/desktop/input-sources/sources "[('xkb', 'us+alt-intl')]"
+dconf write /org/gnome/desktop/peripherals/mouse/accel-profile "'flat'"
+
+## Keybinds
+
+dconf write /org/gnome/desktop/wm/keybindings/switch-applications "@as []"
+dconf write /org/gnome/desktop/wm/keybindings/switch-applications-backward "@as []"
+dconf write /org/gnome/desktop/wm/keybindings/switch-windows "['<Alt>Tab']"
+
+dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/binding "'<Control>t'"
+dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/command "'ptyxis --new-window'"
+dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/name "'Open Terminal'"
+dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']"
+
+# Updating system
+
+sudo dnf update -y
+
+# Installing python pip packages
+
+pip install "${PIP_PACKAGES[@]}"
+
+# Installing Flatpak programs
+
+for program in "${FLATPAK_PROGRAMS[@]}";
+do
+	flatpak install "$program"
+done
+
+# Installing GNOME extensions
+
+sudo dnf install gnome-extensions-app
+
+for extension in "${GNOME_EXTENSIONS[@]}";
+do
+	install_extension "$extension"
+done
+
+# Installing GNOME theme
+
+git clone "$THEME_URL"
+
+theme_name=$(get_repo_name "$THEME_URL")
+
+cd "$theme_name"
+./install.sh -t purple -c dark -s compact -i simple --tweaks black
+cd ..
+rm -rf "$theme_name"
